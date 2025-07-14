d41c08ff983d96ccd7c7e182ef3fbac846195b73
katy <kdiaz@mooba.co>
2025-07-14 09:37:30 -0500

    feat: try a way of save the emails into a google  sheet

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,7 @@
 import { ArrowIcon } from "../ui/Icons";
 import Link from "next/link";
 import Image from "next/image";
+import NewsletterForm from "./InputEmail"
 
 export default function Footer() {
   return (
@@ -34,25 +35,7 @@ export default function Footer() {
         </div>
         
         <div className="flex flex-col w-full">
-          <p className="font-base text-[16px] mb-2">Suscríbete a nuestro newsletter</p>
-          <div className="relative w-full min-w-[200px] mt-3">
-            <input
-              type="email"
-              id="email"
-              placeholder=" "
-              className="peer bg-transparent border-b border-white/30 text-white placeholder-transparent text-sm font-extralight py-2 focus:outline-none focus:border-white/60 transition-colors w-full"
-            />
-            <label
-              htmlFor="email"
-              className="absolute left-0 top-2 text-white/60 text-sm font-extralight transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-sm peer-placeholder-shown:text-white/60 peer-focus:top-[-0.6rem] peer-focus:text-xs peer-focus:text-white"
-            >
-              Correo electrónico
-            </label>
-          </div>
-
-          <div className="bg-white text-black font-medium text-center px-4 py-3 mt-4 rounded-full text-[16px]">
-            Suscribirme
-          </div>
+        <NewsletterForm/>
         </div>
         
         <div className="flex flex-col mt-6 items-center w-full md:flex-row md:items-end md:justify-between">
@@ -83,30 +66,8 @@ export default function Footer() {
           </div>
         </div>
         
-        <div className="flex flex-col items-start">
-          <p className="text-[16px] font-base mb-2">Suscríbete a nuestro newsletter</p>
-          <div className="relative w-full min-w-[400px] mt-3">
-            <input
-              id="email"
-              type="email"
-              placeholder=" "
-              className="peer bg-transparent border-b border-white/30 text-white placeholder-transparent text-sm font-light py-2 focus:outline-none focus:border-white/60 transition-colors w-full"
-            />
-            <label
-              htmlFor="email"
-              className="absolute left-0 top-2 text-white/60 text-sm font-extralight transition-all 
-                peer-placeholder-shown:top-2 
-                peer-placeholder-shown:text-sm 
-                peer-focus:top-[-0.6rem] 
-                peer-focus:text-xs 
-                peer-focus:text-white"
-            >
-              Correo electrónico
-            </label>
-          </div>
-          <div className="bg-white text-black px-4 py-2 mt-4 rounded-full text-sm">
-            Suscríbirme
-          </div>
+        <div className="flex flex-col">
+          <NewsletterForm/>
         </div>
       </div>
       
